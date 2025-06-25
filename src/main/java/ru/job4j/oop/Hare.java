4330ff7958b723fb6f2ee343de967e41964f5682
Alex <kuperalex@gmail.com>
2025-06-25 17:08:25 +0300

    1.6. Взаимодействие объектов [#166087]

---
+++ src/main/java/ru/job4j/oop/Hare.java
@@ -0,0 +1,10 @@
+package ru.job4j.oop;
+
+public class Hare {
+
+    public void tryEat(Ball ball) {
+        System.out.println("Заяц пытается съесть колобка....");
+        ball.tryRun(false);
+    }
+
+}
