f2d018dfa3f33bf0c5655f94e794cd9ab59eab6d
Alex <kuperalex@gmail.com>
2025-07-04 15:47:36 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,25 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book book1 = new Book("Oracle Database WorkBook", 240);
+        Book book2 = new Book("Oracle Database Administrator", 350);
+        Book book3 = new Book("Oracle Database Programmer", 450);
+        Book book4 = new Book("Clean code", 350);
+        Book[] books = new Book[4];
+        books[0] = book1;
+        books[1] = book2;
+        books[2] = book3;
+        books[3] = book4;
+        for (Book book : books) {
+            System.out.println(book.getName() + ": Pages " + book.getCountPages());
+        }
+        System.out.println("Change indexs 0 3");
+        Book temp = books[0];
+        books[0] = books[3];
+        books[3] = temp;
+        for (Book book : books) {
+            System.out.println(book.getName() + ": Pages " + book.getCountPages());
+        }
+    }
+}
